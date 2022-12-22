f5605d9e18d772b2fe6db438bc62daeea78e3f5d
Josh <seapolco@gmail.com>
2022-12-22 14:39:44 +0000

    Added preliminary local storage, about to work on allTodos container

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar';
@@ -7,8 +7,57 @@ import TodoForm from './components/TodoForm';
 
 function App() {
 
-  let [showSidebar, setShowSidebar] = useState(false);
-  let [showForm, setShowForm] = useState(false)
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+
+  const [allTodos, setAllTodos] = useState([])
+
+  function Todo(title, description, priority, date, project) {
+    this.title = title
+    this.description = description
+    this.priority = `Priority: ${priority}`
+    this.date = date
+    this.project = `Project: ${project}`
+  }
+
+
+  // Update the state with any existing local storage todos.
+
+  useEffect(() => {
+    const localStorageTodos = JSON.parse(localStorage.getItem('allTodos'));
+    if (localStorageTodos) {
+     setAllTodos(localStorageTodos);
+    }
+  }, []);
+
+  /// TEST
+
+  const addPomdoro = new Todo(
+    'Pomdoro timer',
+    'Create a pomdoro feature',
+    'High',
+    'Today',
+    'Mustodoit'
+    )
+
+ useEffect(() => {
+
+  setAllTodos((prevTodos) => [...prevTodos, addPomdoro])
+    
+ }, [])
+
+ // May be redundant when functionality is resumed.
+
+ useEffect(() => {
+  localStorage.setItem('allTodos', JSON.stringify(allTodos))
+ },[allTodos])
+
+
+
+ 
+
+ console.log(allTodos)
 
   function handleNavClick () {
     console.log("In the APP")
@@ -20,7 +69,6 @@ function App() {
     setShowForm(!showForm)
   }
 
-  
     return (
       <>
       <Navbar handleNavClick={handleNavClick} handleNewClick={handleNewClick}  />
